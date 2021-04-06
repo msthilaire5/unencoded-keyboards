5363cc508e9e909f478d198cdc293894ca2fbb39
Megan St. Hilaire <mfsthilaire13@gmail.com>
2021-04-06 19:13:19 -0400

    Added server ability to receive and save image

--- main.py
+++ main.py
@@ -6,29 +6,28 @@ Created on Sat Apr  3 10:59:46 2021
 @author: msthilaire
 """
 from flask import Flask, render_template
-
-from datetime import datetime
+from base64 import b64decode
 
 
 # create an instance of Flask. (a "Flask process")
 app = Flask(__name__, template_folder="src/html") 
 
+
 # tell the web server which function to run for "/"
 @app.route('/') 
-def hello_world():
+def drawing_canvas():
     return render_template("test_canvas.html") # return value is sent to the client
 
-# tell the web server which function to run for "/"
-@app.route('/time') 
-def time_page():
-    return """
-    <html>
-  
-    <h1>The time is now...</h1>{time}
-                         test 
-    </html>
-    """.format(time = str(datetime.now()))
 
+@app.route('/upload/<path:img_b64>')
+def upload_char(img_b64):
+    bin_img = b64decode(img_b64)
+    new_char_file = open("new_char.png", 'wb')
+    new_char_file.write(bin_img)
+    new_char_file.close()
+
+    return "Character received!"
+ 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=43560)
+    app.run(debug=True, port=42560)
