842e9f29f08c331d2a6bffd44bb31807cb25e6b6
dr937 <37811849+dr937@users.noreply.github.com>
2021-04-07 21:52:08 -0400

    Updated imagegallery.php

--- imagegallery.php
+++ imagegallery.php
@@ -1,8 +1,13 @@
 <?php
-    $files = scandir('../img/');
+	$x = 1;
+    $files = scandir('./img/');
     foreach($files as $file) {
         if($file !== "." && $file !== "..") {
-            echo "<img src='$file' />";
-        }
+            echo "<input id='btn$x' type='image' src='./img/$file' width='40' height ='40' onclick='input(this);' />";
+        if($x % 8 == 0){
+        	echo "<br />";
+       	}
+            $x++;
+        }           
     }
 ?>
